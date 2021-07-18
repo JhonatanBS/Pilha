df7db72ff67eea2575379ae60cca7b78584ab1fd
Jhonatan Bezerra dos Santos <jhonatanb.s4nt0s@gmail.com>
2021-07-18 19:45:52 -0300

    Adicionado as funções imprime e push na PilhaDinamica.c

--- Pilha/PilhaDinamica.c
+++ Pilha/PilhaDinamica.c
@@ -6,7 +6,45 @@ struct pilha{
     struct pilha* prox;
 };
 
-int main(){
+typedef struct pilha Pilha;
 
+Pilha* push(Pilha* p);
+void imprime(Pilha* p);
+
+int main(){
+    Pilha* P = NULL;
+    P = push(P);
+    P = push(P);
+    imprime(P);
     return 1;
+}
+
+Pilha* push(Pilha* p){
+    Pilha* novo = (Pilha*)malloc(sizeof(Pilha));
+
+    if(novo == NULL){
+        printf("Erro na alocação de Memória!");
+        return 0;
+    }
+
+    novo->prox=p;
+    
+    printf("Digite um valor:");
+    scanf("%d", &novo->valor);
+    
+    return novo;
+
+}
+
+void imprime(Pilha* p){
+    Pilha *aux = p;
+
+    printf("\nPilha:\n");
+
+    while(aux != NULL){
+        printf("\n|%d|", aux->valor);
+        aux = aux->prox;
+    }
+    printf("-> NULL");
+
 }
