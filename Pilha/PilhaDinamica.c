1dad5f8d6e11605998ce1a53fa5ec7a1f4583518
Jhonatan Bezerra dos Santos <jhonatanb.s4nt0s@gmail.com>
2021-07-18 21:45:20 -0300

    Foram criadas as funções: pop,menu e adicionado as opções do menu

--- Pilha/PilhaDinamica.c
+++ Pilha/PilhaDinamica.c
@@ -9,42 +9,76 @@ struct pilha{
 typedef struct pilha Pilha;
 
 Pilha* push(Pilha* p);
+Pilha* pop(Pilha* p);
 void imprime(Pilha* p);
+void menu();
 
 int main(){
-    Pilha* P = NULL;
-    P = push(P);
-    P = push(P);
-    imprime(P);
-    return 1;
+     Pilha* P = NULL;
+     int opcao;
+
+         do{
+             menu();
+             printf("\nEscolha uma opção:");
+             scanf("%d", &opcao);
+
+ switch(opcao){
+         case 0:
+         printf("\nPilha encerrada!");
+             break;
+         case 1:
+         P = push(P);
+             break;
+         case 2:
+         P = pop(P);
+             break;
+         case 3:
+         imprime(P);
+             break;
+              }
+         }while(opcao != 0);
+
+ return 1;
 }
 
 Pilha* push(Pilha* p){
     Pilha* novo = (Pilha*)malloc(sizeof(Pilha));
+     if(novo == NULL){
+         printf("Erro na alocação de Memória!");
+         return 0;
+     }
+     novo->prox=p;
+         printf("Digite um valor:");
+         scanf("%d", &novo->valor);
+ return novo;
+}
 
-    if(novo == NULL){
-        printf("Erro na alocação de Memória!");
-        return 0;
+Pilha* pop(Pilha* p){
+    if(p == NULL){
+        printf("\nErro na remoção: A pilha está vazia!");
+        return p;
     }
-
-    novo->prox=p;
-    
-    printf("Digite um valor:");
-    scanf("%d", &novo->valor);
-    
-    return novo;
-
+     Pilha* aux = p;
+     p = p->prox;
+     free(aux);
+ return p;
 }
 
 void imprime(Pilha* p){
-    Pilha *aux = p;
-
-    printf("\nPilha:\n");
-
-    while(aux != NULL){
-        printf("\n|%d|", aux->valor);
-        aux = aux->prox;
+     Pilha *aux = p;
+     if(p == NULL){
+         printf("Pilha Vazia!\n");
+         return p;
     }
-    printf("-> NULL");
+         printf("\nPilha:\n");
 
+     while(aux != NULL){
+         printf("\n|%d|", aux->valor);
+         aux = aux->prox;
+    }
+         printf("-> NULL");
+}
+
+void menu(){
+    printf("\n\n Menu\n 0 - Sair\n 1 - Push\n 2 - Pop\n 3 - Imprime\n");
 }
